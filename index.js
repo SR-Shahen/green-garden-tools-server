0c20b6d2dd643c02f133572583e313920f92c989
SR-Shahen <s.r.shahenda@gmail.com>
2022-07-13 20:41:31 +0600

    create get api to collect data from database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 // Use Middleware
@@ -10,21 +10,29 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ohevh.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.guzoq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri);
 console.log('database connected ');
 
 async function run() {
     try {
         client.connect();
+        const toolsCollection = client.db("green-garden-tools").collection("tools");
 
+        // Get Tools
+        app.get('/tool', async (req, res) => {
+            const query = {};
+            const tools = await toolsCollection.find().toArray();
+            res.send(tools);
+        })
     }
     finally {
 
     }
 }
-
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
