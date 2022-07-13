c521195f6d09a9708a944d18360bd1156c84228a
SR-Shahen <s.r.shahenda@gmail.com>
2022-07-13 21:05:31 +0600

    create dynamic api to collect single tool data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 // Use Middleware
@@ -26,7 +26,15 @@ async function run() {
             const query = {};
             const tools = await toolsCollection.find().toArray();
             res.send(tools);
+
+            app.get('/tool/:id', async (req, res) => {
+                const id = req.params.id;
+                const query = { _id: ObjectId(id) };
+                const tools = await toolsCollection.findOne(query);
+                res.send(tools);
+            })
         })
+
     }
     finally {
 
