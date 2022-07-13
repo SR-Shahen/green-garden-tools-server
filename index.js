3993ba3bb82ec2274a5d9e02fdadb6d4e2a29f0a
SR-Shahen <s.r.shahenda@gmail.com>
2022-07-13 20:09:19 +0600

    connected with database

--- index.js
+++ index.js
@@ -2,13 +2,28 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
-
 require('dotenv').config()
 // Use Middleware
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ohevh.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('database connected ');
+
+async function run() {
+    try {
+        client.connect();
+
+    }
+    finally {
+
+    }
+}
 
 
 
